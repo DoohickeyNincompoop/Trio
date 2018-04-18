bb80aa4dfb23f1794e576b0e64d8e1dcd3505809
Stuy-Kevin-Lin <klin14@stuy.edu>
2018-04-18 08:38:22 -0400

    Interface

--- Deque.java
+++ Deque.java
@@ -1,3 +1,14 @@
 public interface Deque<Card> {
+    
+    public void addFirst(Card e);
 
+    public Card removeFirst();
+
+    public Card peekFirst();
+
+    public void addLast(Card e);
+
+    public Card removeLast();
+
+    public Card peekLast();
 }
