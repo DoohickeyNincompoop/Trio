04d29f6cd0842a23692377f087bddd6b877d507a
Qian Zhou <qzhou@stuy.edu>
2018-04-25 17:18:37 -0400

    driver and two methods printing full collection for testing

--- QQKachoo.java
+++ QQKachoo.java
@@ -1,105 +1,134 @@
 public class QQKachoo<D> implements Deque<D> {
 
     private DLLNode<D> _front, _end;
-  private int _size;
+    private int _size;
 
-  public QQKachoo() {
-    _front = null;
-    _end = null;
-    _size = 0;
-  }
-
-  public void addFirst(D element) {
-    DLLNode<D> input = new DLLNode<D>(element,null,_front);
-    if (_front == null) {//Empty Queue
-      _front = input;
-      _end = _front;
+    public QQKachoo() {
+	_front = null;
+	_end = null;
+	_size = 0;
     }
-    else {
-      _front.setPrev(input);
-      _front = input;
+
+    public void addFirst(D element) {
+	DLLNode<D> input = new DLLNode<D>(element,null,_front);
+	if (_front == null) {//Empty Queue
+	    _front = input;
+	    _end = _front;
+	}
+	else {
+	    _front.setPrev(input);
+	    _front = input;
+	}
+	_size++;
     }
-    _size++;
-  }
 
-  public D removeFirst() {
-    if (isEmpty()) return null;
-    D output = _front.getCargo();
-    if (_size == 1) {//Queue with a size of 1
-      _end = null;
-      _front = null;
+    public D removeFirst() {
+	if (isEmpty()) return null;
+	D output = _front.getCargo();
+	if (_size == 1) {//Queue with a size of 1
+	    _end = null;
+	    _front = null;
+	}
+	else {//Every other size
+	    _front = _front.getNext();
+	    _front.setPrev(null);
+	}
+	_size--;
+	return output;
     }
-    else {//Every other size
-      _front = _front.getNext();
-      _front.setPrev(null);
+
+    public D peekFirst() {return _front.getCargo();}
+
+    public void addLast(D element) {
+	DLLNode<D> input = new DLLNode<D>(element,_end,null);
+	if (isEmpty()) {
+	    _front = input;
+	    _end = input;
+	}
+	else {
+	    _end.setNext(input);
+	    _end = input;
+	}
+	_size++;
     }
-    _size--;
-    return output;
-  }
 
-  public D peekFirst() {return _front.getCargo();}
+    public D removeLast() {
+	if (_size == 0) return null;
+	D temp = _end.getCargo();
+	if (_size == 1){
+	    _front = null;
+	    _end = null;
+	}
+	else{
+	    _end = _end.getPrev();
+	    _end.setNext(null);
+	}
+	_size--;
+	return temp;
+    }
 
-  public void addLast(D element) {
-      DLLNode<D> input = new DLLNode<D>(element,_end,null);
-      if (isEmpty()) {
-	  _front = input;
-	  _end = input;
-      }
-      else {
-	  _end.setNext(input);
-	  _end = input;
-      }
-      _size++;
-  }
+    public D peekLast() {
+	return _end.getCargo();
+    }
 
-  public D removeLast() {
-      if (_size == 0) return null;
-      D temp = _end.getCargo();
-      if (_size == 1){
-	  _front = null;
-	  _end = null;
-      }
-      else{
-	  _end = _end.getPrev();
-	  _end.setNext(null);
-      }
-      _size--;
-      return temp;
-  }
+    public boolean isEmpty() {return _size == 0;}
 
-  public D peekLast() {
-      return _end.getCargo();
-  }
+    public int size() {return _size;}
 
-  public boolean isEmpty() {return _size == 0;}
 
-  public int size() {return _size;}
+    //vvvvvvvvvvvv----implementations for review, JungleJuice-------vvvvvvvvvvv
+    //toString mmethod for testing
+    public String toString(){
+	String s="Start-->";
+	DLLNode temp = _front;
+	while (temp!=null){
+	    s+=temp.getCargo()+"-->";
+	    temp=temp.getNext();
+	    
+	}
+	s+="End";
+	return s;
 	
-  public static void main(String[] args){
+    }
+    //to test robustity of backward link
+    public String backString(){
+	String s="-->End";
+	DLLNode temp = _end;
+	while (temp!=null){
+	    s="-->"+temp.getCargo()+s;
+	    temp=temp.getPrev();
+	    
+	}
+	s="Start"+s;
+	return s;
+    }
+    //^^^^^^^^^^^^^^^^^-------------------^^^^^^^^^^^^^^^^^^^^
+    
+    public static void main(String[] args){
 	  
 	QQKachoo<String> pow = new QQKachoo<String>();
-	  pow.addFirst("one");
-	  pow.addFirst("two");
-	  pow.addFirst("three");
+	pow.addFirst("one");
+	pow.addFirst("two");
+	pow.addFirst("three");
 	  
-	  pow.peekFirst();
+	pow.peekFirst();
 	  
-	  pow.removeFirst();
+	pow.removeFirst();
 	  
-	  pow.peekFirst();
+	pow.peekFirst();
 	  
-	  pow.peekLast();
+	pow.peekLast();
 	  
-	  pow.removeLast();
+	pow.removeLast();
 	  
-	  pow.peekFirst();
-	  pow.peekLast();
+	pow.peekFirst();
+	pow.peekLast();
 	  
-	  pow.size();
+	pow.size();
 	  
-	  pow.isEmpty();
+	pow.isEmpty();
 	  
-	  }
+    }
 		      
 }
 
